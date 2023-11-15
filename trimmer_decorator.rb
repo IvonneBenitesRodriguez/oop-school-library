eb448be4f3705fce76fe3e9e5d9ab02f1fb7141a
Ivonne <ivonnebenites2015@gmail.com>
2023-11-15 00:07:24 -0500

    Update file

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,6 +3,6 @@ require_relative 'decorator'
 # creating a class that inherits Decorator class features
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name[0, 10]
+    @nameable.correct_name[0..9]
   end
 end
