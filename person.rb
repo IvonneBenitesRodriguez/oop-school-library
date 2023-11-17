5ae93ec80bf47ff22a06614d4539bbf985fd7e54
Ivonne <ivonnebenites2015@gmail.com>
2023-11-16 19:17:45 -0500

    Update file named person.rb

--- person.rb
+++ person.rb
@@ -43,7 +43,7 @@ class Person < Nameable
   end
 end
 
-person = Person.new(22, 'maximilianus')
+person = Person.new('maximilianus', 22)
 puts person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
