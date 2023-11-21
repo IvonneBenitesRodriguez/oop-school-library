3ead6ddf8729b44cbe982c1b94ebd3b55bd41fb2
Ivonne <ivonnebenites2015@gmail.com>
2023-11-20 20:01:34 -0500

    Update file named person.rb

--- person.rb
+++ person.rb
@@ -32,10 +32,3 @@ class Person < Nameable
     @name
   end
 end
-
-person = Person.new('maximilianus', 22)
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
