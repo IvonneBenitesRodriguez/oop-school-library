d8d67ba12d4c5d5e71b617aeef5be6ee38098ea8
Ivonne <ivonnebenites2015@gmail.com>
2023-11-21 22:46:30 -0500

    Update file

--- rental.rb
+++ rental.rb
@@ -1,13 +1,29 @@
 # creating a class named Rental
 class Rental
-  attr_accessor :date, :book, :person
+  attr_accessor :date
+  attr_reader :book, :person
 
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
+  end
 
-    book.rentals << self
-    person.rentals << self
+  def book=(book)
+    @book = book
+    book.add_rental(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.add_rental(self)
+  end
+
+  def to_h
+    {
+      date: @date,
+      book_title: @book.title,
+      person_id: @person.id
+    }
   end
 end
