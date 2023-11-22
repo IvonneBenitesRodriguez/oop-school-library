95ede472785ccd54b357d898cd74781ec7bf8c55
gilberthappi <gdushimimana6@gmail.com>
2023-11-22 10:08:54 +0200

    pdating modules

--- modules/books_data_manipulation.rb
+++ modules/books_data_manipulation.rb
@@ -1,23 +1,23 @@
-# require 'json'
+require 'json'
 
-# module BooksDataManipulation
-#   def save_books_to_json(file_path, new_book)
-#     parsed_data = []
+module BooksDataManipulation
+  def save_books_to_json(file_path, new_book)
+    parsed_data = []
 
-#     if File.exist?(file_path)
-#       current_data = File.read(file_path)
-#       parsed_data = JSON.parse(current_data) unless current_data.empty?
-#     end
+    if File.exist?(file_path)
+      current_data = File.read(file_path)
+      parsed_data = JSON.parse(current_data) unless current_data.empty?
+    end
 
-#     book_data = {
-#       title: new_book.title,
-#       author: new_book.author
-#     }
+    book_data = {
+      title: new_book.title,
+      author: new_book.author
+    }
 
-#     parsed_data << book_data
+    parsed_data << book_data
 
-#     File.write(file_path, JSON.pretty_generate(parsed_data))
-#   end
+    File.write(file_path, JSON.pretty_generate(parsed_data))
+  end
 
   def load_books_from_json(file_path)
     if File.exist?(file_path)
--- modules/people_data_manipulation.rb
+++ modules/people_data_manipulation.rb
@@ -20,16 +20,16 @@ module PeopleDataManipulation
     File.write(file_path, JSON.pretty_generate(parsed_data))
   end
 
-#   def load_people_from_json(file_path)
-#     if File.exist?(file_path)
-#       current_data = File.read(file_path)
-#       parsed_data = JSON.parse(current_data) unless current_data.empty?
-#       return [] if parsed_data.nil?
-#     end
+  def load_people_from_json(file_path)
+    if File.exist?(file_path)
+      current_data = File.read(file_path)
+      parsed_data = JSON.parse(current_data) unless current_data.empty?
+      return [] if parsed_data.nil?
+    end
 
-#     parsed_data.map do |person_data|
-#       person = Person.new(person_data['name'], person_data['age'], person_data['id'])
-#       person
-#     end
-#   end
+    parsed_data.map do |person_data|
+      person = Person.new(person_data['name'], person_data['age'], person_data['id'])
+      person
+    end
+  end
 end
