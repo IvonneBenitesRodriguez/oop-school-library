d8980fd7f5da34f1276bd45a2d84fb12c6ac3a36
Ivonne <ivonnebenites2015@gmail.com>
2023-11-21 22:48:05 -0500

    Add file

---
+++ modules/books_data_manipulation.rb
@@ -0,0 +1,34 @@
+# require 'json'
+
+# module BooksDataManipulation
+#   def save_books_to_json(file_path, new_book)
+#     parsed_data = []
+
+#     if File.exist?(file_path)
+#       current_data = File.read(file_path)
+#       parsed_data = JSON.parse(current_data) unless current_data.empty?
+#     end
+
+#     book_data = {
+#       title: new_book.title,
+#       author: new_book.author
+#     }
+
+#     parsed_data << book_data
+
+#     File.write(file_path, JSON.pretty_generate(parsed_data))
+#   end
+
+  def load_books_from_json(file_path)
+    if File.exist?(file_path)
+      current_data = File.read(file_path)
+      parsed_data = JSON.parse(current_data) unless current_data.empty?
+      return [] if parsed_data.nil?
+    end
+
+    parsed_data.map do |book_data|
+      book = Book.new(book_data['title'], book_data['author'])
+      book
+    end
+  end
+end
